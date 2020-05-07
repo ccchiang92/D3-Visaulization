e09b147f3b151a94789688a902e4b35bacfb4574
jason-hernandez-73 <56489096+jason-hernandez-73@users.noreply.github.com>
2020-05-07 10:10:11 -0700

    Update summary.py

--- cleaned_data/In_progress/summary.py
+++ cleaned_data/In_progress/summary.py
@@ -6,6 +6,10 @@ import os
 def summarize():
     path=os.getcwd()
     sars_file_path='SARS_data.csv'
+    mers_file_path='mers_final.csv'
+    ebola_file_path='ebola_final.csv'
+    covid_file_path='covid_daily_world.csv'
+
     # list of dictionary where each dictionary holds the stats for each disease
     all_disease=[]
 
@@ -14,23 +18,52 @@ def summarize():
     df=pd.read_csv(path+'/'+sars_file_path)
     latest_date=df['Date'].max()
     sars_max_case_ct=df[(df['Date']==latest_date) & (df['Country']=='Total')]['Cases'].values[0]
+    sars_max_death_ct=df[(df['Date']==latest_date) & (df['Country']=='Total')]['Deaths'].values[0]
 
     sars_dict['name']='sars'
-    sars_dict['case']=sars_max_case_ct
-    # print(df[df['Country']=='Total'])
-    #sars_dict={'name': 'SARS', 
-    #        'death_count': 100, 
-    #        'case_count': 50}
+    sars_dict['cases']=sars_max_case_ct
+    sars_dict['deaths']=sars_max_death_ct
 
     all_disease.append(sars_dict)
+    
+    # Summarize MERS
+    mers_dict={}
+    df=pd.read_csv(path+'/'+mers_file_path)
+    mers_max_case_ct=df['Cases'].sum()
 
+    sars_dict['name']='mers'
+    sars_dict['cases']=sars_max_case_ct
+    sars_dict['deaths']='nd'
 
-    # df=pd.read_csv(path+'/'+sars_file_path)
-    # print(df)
+    all_disease.append(mers_dict)
 
-    # files=os.listdir(path)
+    # summarize ebola
+    ebola_dict={}
+    df=pd.read_csv(path+'/'+ebola_file_path)
+    latest_date=df['Date'].max()
+    ebola_max_case_ct=df[(df['Date']==latest_date)]['No. of confirmed cases'].sum
+    ebola_max_death_ct=df[(df['Date']==latest_date)['No. of confirmed deaths'].sum
 
-    # files=[file_name for file_name in files if file_name.split('.')[-1] == 'csv']
-    # for each_file in files: 
-        # do something with the csv, e.g. pd.read_csv()
+    ebola_dict['name']='ebola'
+    ebola_dict['cases']=ebola_max_case_ct
+    ebola_dict['deaths']=ebola_max_death_ct
+
+    all_disease.append(ebola_dict)
+
+    # summarize COVID
+    covid_dict={}
+    df=pd.read_csv(path+'/'+covid_file_path)
+    latest_date=df['Date'].max()
+    covid_max_case_ct=df[(df['Date']==latest_date) & (df['Country']=='Total')]['Confirmed'].values[0]
+    covid_max_death_ct=df[(df['Date']==latest_date) & (df['Country']=='Total')]['Deaths'].values[0]
+
+    covid_dict['name']='covid-19'
+    covid_dict['cases']=covid_max_case_ct
+    covid_dict['deaths']=covid_max_death_ct
+
+    all_disease.append(covid_dict)
+
+    # Create new CSV for summary table
+    all_disease.to_csv(path+'/summary_table.csv')
+
 summarize()
