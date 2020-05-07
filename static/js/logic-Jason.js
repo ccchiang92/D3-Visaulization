531bc26becb3e09ead1ce6ec62586386c299c2d2
jason-hernandez-73 <56489096+jason-hernandez-73@users.noreply.github.com>
2020-05-06 20:03:42 -0700

    Update logic-Jason.js

--- static/js/logic-Jason.js
+++ static/js/logic-Jason.js
@@ -1,5 +1,36 @@
+// Call CSV data
+d3.csv("assets/data/data.csv").then(function (chartData) {
+    chartData.forEach(function (dataPoint) {
+        dataPoint.index = +dataPoint.index;
+        dataPoint.index2 = +dataPoint.index2;
+    });
+
+//Listener for button clicks.
 button.on('click', function() {
     var inputElement = d3.select(html_element);
     var inputValue = inputElement.property('value');
-    d3.select('h1>span').text(inputValue);
+    function filterData(input){
+        return input.virus == inputValue
+    };
+    var filteredData = data.filter(filterData);
+    populate(filteredData);
 });
+
+// Create summary table
+function populate(arr) {
+    var table = d3.select("tbody");
+    table.attr("id", "table");
+    table.html("");
+    arr.forEach(function (obj) {
+        row = table.append("tr");
+        row.append("td").text(obj.datetime);
+        row.append("td").text(obj.city);
+        row.append("td").text(obj.state);
+        row.append("td").text(obj.country);
+        row.append("td").text(obj.shape);
+        row.append("td").text(obj.durationMinutes);
+        row.append("td").text(obj.comments);
+    })
+}
+// Populate table
+populate(data);
