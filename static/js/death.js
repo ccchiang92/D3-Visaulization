1cdf0568b61d0fd5fda1e18d424942944376ae6f
Sowmya Srinivasan <43221282+sowmyasrinivasan@users.noreply.github.com>
2020-05-09 01:09:08 -0700

    line graphs and pie chart

---
+++ static/js/death.js
@@ -0,0 +1,85 @@
+d3.csv("deaths.csv").then(function(virus) {
+
+    console.log(virus);
+
+    var days = [],
+        sars = [],
+        ebola = [],
+        covid = [];
+
+    virus.forEach(function(data) {
+        data.days = parseInt(data.days);
+        data.ebola = parseInt(data.ebola_death);
+        data.covid = parseInt(data.covid_death);
+        data.sars = parseInt(data.sars_death);
+        days.push(data.days);
+        sars.push(data.sars);
+        ebola.push(data.ebola);
+        covid.push(data.covid);
+    });
+
+    var trace1 = {
+        x: days,
+        y: sars,
+        name: "SARS",
+        type: "line"
+    };
+
+    var trace2 = {
+        x: days,
+        y: covid,
+        name: "COVID",
+        type: "line"
+    };
+
+    var trace3 = {
+        x: days,
+        y: ebola,
+        name: "Ebola",
+        type: "line"
+    };
+
+    var data = [trace1, trace2, trace3];
+
+    var layout = {
+        title: {
+            text:'Number of Deaths over Time',
+            font: {
+                size: 18
+            },
+        },
+
+        yaxis: {
+            title: {
+                text: 'Number of Deaths',
+                font: {
+                    size: 13
+                }
+            },
+            range: [0, 100000]
+        },
+        xaxis: {
+            title: {
+                text: 'Days Since First Case',
+                font: {
+                    size: 13
+                }
+            },
+        },
+        autosize: false,
+        width: 400,
+        height: 300,
+        margin: {
+        l: 80,
+        r: 50,
+        pad: 10
+        },
+    };
+
+    Plotly.newPlot("plt", data, layout);
+
+  
+
+}).catch(function(error) {
+  console.log(error);
+});
