3435aea480a9c83ec8b92dedadaa1c36b6889dcc
jason-hernandez-73 <56489096+jason-hernandez-73@users.noreply.github.com>
2020-05-10 16:03:22 -0700

    Summary table

--- static/js/logic-Jason.js
+++ static/js/logic-Jason.js
@@ -1,22 +1,14 @@
 // Call CSV data
-d3.csv("static/data/summary_table.csv")
-//.then(function (chartData) {
-    //D={
-        // StackOverflow used this to sum a selected column
-       // slice1:d3.sum(data, function(d){return parseFloat(d.cases);}),
-    //};
+d3.csv("static/data/summary_table.csv", function(chartData){
 
 // Create summary table
-function populate(arr) {
-    var table = d3.select("tbody");
-    table.attr("id", "table");
+
+    var table = d3.select("#table");
     table.html("");
-    arr.forEach(function (obj) {
+    chartData.forEach(function (obj) {
         row = table.append("tr");
         row.append("td").text(obj.virus);
         row.append("td").text(obj.cases);
         row.append("td").text(obj.deaths);
-    })
-}
-// Populate table
-populate(data);
+    });
+});
