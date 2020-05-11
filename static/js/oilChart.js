bde0b35b07df3a650a82b92ba7e784a646d47b0a
jason-hernandez-73 <56489096+jason-hernandez-73@users.noreply.github.com>
2020-05-11 13:35:10 -0700

    Oil Chart Done!

---
+++ static/js/oilChart.js
@@ -0,0 +1,64 @@
+let csv_data;
+
+d3.csv('./static/data/oil_prices.csv', function (error, oilData) {
+    csv_data = oilData;
+    console.log(csv_data)
+    init()
+});
+
+function oilChange(virus) {
+    oilChart(virus)
+}
+
+function init() {
+    const selector = d3.select("#oil-dropdown");
+    let viruses = [];
+    const virusData = csv_data.map(row => row.Virus);
+    virusData.forEach(v => {
+        if (!viruses.includes(v)) {
+            viruses.push(v);
+        };
+    });
+    viruses.forEach(v => {
+        selector.append("option")
+            .property("value", v)
+            .text(v);
+    });
+    oilChange(viruses[0])
+}
+
+function oilChart(virus) {
+    // Code from https://plotly.com/javascript/multiple-axes/
+    const filteredData = csv_data.filter(row => row.Virus === virus);
+    console.log(filteredData);
+    var trace1 = {
+        x: filteredData.map(row => row.Date),
+        y: filteredData.map(row => row.Brent_Spot_Price),
+        name: 'weekly oil price',
+        type: 'scatter'
+      };
+      
+      var trace2 = {
+        x: filteredData.map(row => row.Date),
+        y: filteredData.map(row => row.Cases),
+        name: 'number of cases',
+        yaxis: 'y2',
+        type: 'scatter'
+      };
+    
+    var data = [trace1, trace2];
+
+    var layout = {
+        title: 'Oil Prices During Viral Outbreaks',
+        yaxis: { title: 'Number of cases and deaths' },
+        yaxis2: {
+            title: 'Weekly oil price',
+            titlefont: { color: 'rgb(148, 103, 189)' },
+            tickfont: { color: 'rgb(148, 103, 189)' },
+            overlaying: 'y',
+            side: 'right'
+        }
+    };
+    Plotly.newPlot('oil-chart', data, layout)
+}
+
