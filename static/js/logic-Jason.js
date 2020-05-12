66c98f7cb35fda173279f7f6081d91f6b3767a1d
Chris Chiang <christopherchiang92@gmail.com>
2020-05-11 19:10:27 -0700

    summary

--- static/js/logic-Jason.js
+++ static/js/logic-Jason.js
@@ -13,10 +13,17 @@ d3.csv("static/data/summary_table.csv", function(chartData){
     var i =0;
     chartData.forEach(function (obj) {
         row = table.append("tr");
+<<<<<<< HEAD
         row.append("td").style("color",colors[i]).text(obj.virus);
         row.append("td").style("color",colors[i]).text(obj.cases);
         row.append("td").style("color",colors[i]).text(obj.deaths);
         row.append("td").style("color",colors[i]).text((obj.deaths / obj.cases*100).toFixed(2)+'%');
         i++;
+=======
+        row.append("td").text(obj.virus);
+        row.append("td").text(obj.cases);
+        row.append("td").text(obj.deaths);
+        row.append("td").text(`${(obj.deaths / obj.cases).toFixed(2)}%`);
+>>>>>>> c38ba38e1b532fefb1389459e5bdc124a5a39ce5
     });
 });
